d61d0f594c29179a6b9527e12dfa85ea922cec73
Vanessa <vanessa.y.cui@gmail.com>
2022-12-19 13:57:01 -0500

    adding authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     #general views
     path('', views.home, name='home'),
     path('create/', views.create, name='meme_create'),
+    #sign_up
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Meme, Comment
+# Add the two imports below
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+# Import the login_required decorator
+from django.contrib.auth.decorators import login_required
+# Import the mixin for class-based views
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
+    # memes = Meme.objects.all() #pulling all memes from our db
     return render(request, 'home.html')
 
 def create(request):
     return render(request, 'memes/create.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST': 
+    form = UserCreationForm(request.POST) 
+    if form.is_valid():
+      user = form.save()
+      login(request,user)
+      return redirect('index')
+    else:
+      print(form.errors)
+      error_message=form.errors
+
+  #if it's a get request or it's a bad post...    
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message }
+  return render(request, 'registration/signup.html', {'form': form, 'error_message': error_message})
