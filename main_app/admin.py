cc263ff8237309af5c1b823eaa76784da84b9f37
Vanessa <vanessa.y.cui@gmail.com>
2022-12-19 11:00:54 -0500

    registering models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Meme, Comment
 
 # Register your models here.
+admin.site.register(Meme)
+admin.site.register(Comment)
--- memerton_post/settings.py
+++ memerton_post/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'memerton_post.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'memerton-post',
+        'NAME': 'memertonpost',
     }
 }
 
