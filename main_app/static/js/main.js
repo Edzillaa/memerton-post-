daa0aa3b0f54ec0d37a14a3f6450a66331902d40
Vanessa <vanessa.y.cui@gmail.com>
2023-01-03 10:43:52 -0500

    modified model to include confidence field, adding dropdown menu to index

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -85,3 +85,11 @@ listeners: {
     },
 }
 })
+
+
+//initialization for materialize dropdown
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
