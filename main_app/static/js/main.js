afddbb58a0a3948f24a10e8ceb1ee4c0e825e05f
Vanessa <vanessa.y.cui@gmail.com>
2023-01-04 15:50:20 -0500

    minor modifications to css and js for create page

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -12,6 +12,7 @@ buttonDone.addEventListener('click', (event)=>{
     event.preventDefault()
     html2canvas(document.querySelector("#capture"), {backgroundColor: "rgba(0,0,0,0)", allowTaint: true, useCORS: true}).then(canvas => {
         let img= canvas.toDataURL("img/jpg");
+        imagePanel.appendChild()
         uploadMemeForm.style.display="block"
         uploadMemeData.value = img
         buttonDone.style.display="none"      
